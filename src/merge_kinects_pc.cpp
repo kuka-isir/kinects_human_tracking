bae3a642e4fe21b377e425665f7c5ea1c0bb3a6c
JimmyDaSilva <jimmy.dasilva@isir.upmc.fr>
2015-08-26 11:27:09 +0200

    Multi-kinect merge working with 3 kinects

--- src/merge_kinects_pc.cpp
+++ src/merge_kinects_pc.cpp
@@ -45,21 +45,22 @@ void callback3(const PCMsg::ConstPtr& first_msg_pc2, const PCMsg::ConstPtr& seco
 void callback2(const PCMsg::ConstPtr& first_msg_pc2, const PCMsg::ConstPtr& second_msg_pc2);
 void callback(const PCMsg::ConstPtr& first_msg_pc2);
 void merge_pcs(vector<PCMsg::ConstPtr> pcs_msg_pc2);
+void get_transform(string pc_frame, pclTransform &pc_transform);
 
-pclTransform first_transform, second_transform;
-bool first_frame;
-string out_frame, out_topic_name;
-vector<string> in_topic_names;
-ros::Publisher pc_pub;
-vector<PointCloudSM::Ptr> pcs;
-vector<PCMsg::ConstPtr> pcs_msg_pc2;
-PointCloudSM::Ptr merged_pc;
+vector<pclTransform> transforms_;
+bool first_frame_;
+string out_frame_, out_topic_name_;
+vector<string> in_topic_names_;
+ros::Publisher pc_pub_;
+vector<PointCloudSM::Ptr> pcs_;
+vector<PCMsg::ConstPtr> pcs_msg_pc2_;
+PointCloudSM::Ptr merged_pc_;
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "merge_kinects_pc");
   ros::NodeHandle nh, nh_priv("~");
   
-  first_frame = true;
+  first_frame_ = true;
   
   // Get params topics and frames names
   int nb_topics = 0;
@@ -71,39 +72,39 @@ int main(int argc, char** argv){
     done_loading = (!nh_priv.getParam(topic_name, temp)) || (temp=="") ;
     
     if(!done_loading){
-      in_topic_names.push_back(temp);
+      in_topic_names_.push_back(temp);
       cout << temp <<endl;
       nb_topics++;
     }
     
-  }while(!done_loading);
-  cout << endl;
-  cout << "Merging "<<nb_topics<<" topics"<<endl<<endl;
+  }while(!done_loading);  
+  nh_priv.getParam("out_frame", out_frame_);
+  nh_priv.getParam("out_topic_name", out_topic_name_);
+  cout << "Merging "<<nb_topics<<" topics to topic "<<out_topic_name_<<" in frame "<<out_frame_<<endl;
   
-  nh_priv.getParam("out_frame", out_frame);
-  nh_priv.getParam("out_topic_name", out_topic_name);
+  transforms_.resize(nb_topics);
   
   // Initialize shared pointers and reserve memory
   for (int i=0; i<nb_topics;i++){
-    pcs.push_back(boost::shared_ptr<PointCloudSM>(new PointCloudSM));
-    pcs[i]->reserve(10000);
+    pcs_.push_back(boost::shared_ptr<PointCloudSM>(new PointCloudSM));
+    pcs_[i]->reserve(10000);
   }
-  merged_pc = boost::shared_ptr<PointCloudSM>(new PointCloudSM); 
-  merged_pc->reserve(10000);
+  merged_pc_ = boost::shared_ptr<PointCloudSM>(new PointCloudSM); 
+  merged_pc_->reserve(10000);
   
   // Ros Subscribers and Publishers
-  pc_pub = nh.advertise<PointCloudSM>(out_topic_name, 1);
+  pc_pub_ = nh.advertise<PointCloudSM>(out_topic_name_, 1);
   
   vector<boost::shared_ptr<message_filters::Subscriber<PCMsg> > > subs(nb_topics);
   for (int i=0; i<nb_topics; i++)
-    subs[i] = boost::shared_ptr<message_filters::Subscriber<PCMsg> >(new message_filters::Subscriber<PCMsg>(nh, in_topic_names[i],1));
+    subs[i] = boost::shared_ptr<message_filters::Subscriber<PCMsg> >(new message_filters::Subscriber<PCMsg>(nh, in_topic_names_[i],1));
   
   switch( nb_topics ){
     case 0: 
       ROS_ERROR("No topics to merge provided");
       return 1; break;
     case 1:{
-      ros::Subscriber pc_sub = nh.subscribe<PCMsg>(in_topic_names[0], 1, callback);
+      ros::Subscriber pc_sub = nh.subscribe<PCMsg>(in_topic_names_[0], 1, callback);
       ros::spin();
       break;
     }
@@ -142,49 +143,84 @@ int main(int argc, char** argv){
 }
 
 void callback(const PCMsg::ConstPtr& first_msg_pc2){
-  cout <<"in"<<endl;
-  pcs_msg_pc2.clear();
-  pcs_msg_pc2.push_back(first_msg_pc2);
-  merge_pcs(pcs_msg_pc2);
+  pcs_msg_pc2_.clear();
+  pcs_msg_pc2_.push_back(first_msg_pc2);
+  merge_pcs(pcs_msg_pc2_);
 }
 
 void callback2(const PCMsg::ConstPtr& first_msg_pc2, const PCMsg::ConstPtr& second_msg_pc2){
-  cout <<"in"<<endl;
-  pcs_msg_pc2.clear();
-  pcs_msg_pc2.push_back(first_msg_pc2);
-  pcs_msg_pc2.push_back(second_msg_pc2);
-  merge_pcs(pcs_msg_pc2);
+  pcs_msg_pc2_.clear();
+  pcs_msg_pc2_.push_back(first_msg_pc2);
+  pcs_msg_pc2_.push_back(second_msg_pc2);
+  merge_pcs(pcs_msg_pc2_);
 }
 
 void callback3(const PCMsg::ConstPtr& first_msg_pc2, const PCMsg::ConstPtr& second_msg_pc2, const PCMsg::ConstPtr& third_msg_pc2 ){
-  cout <<"in"<<endl;
-  pcs_msg_pc2.clear();
-  pcs_msg_pc2.push_back(first_msg_pc2);
-  pcs_msg_pc2.push_back(second_msg_pc2);
-  pcs_msg_pc2.push_back(third_msg_pc2);
-  merge_pcs(pcs_msg_pc2);
+  pcs_msg_pc2_.clear();
+  pcs_msg_pc2_.push_back(first_msg_pc2);
+  pcs_msg_pc2_.push_back(second_msg_pc2);
+  pcs_msg_pc2_.push_back(third_msg_pc2);
+  merge_pcs(pcs_msg_pc2_);
 }
 void callback4(const PCMsg::ConstPtr& first_msg_pc2, const PCMsg::ConstPtr& second_msg_pc2, const PCMsg::ConstPtr& third_msg_pc2, const PCMsg::ConstPtr& fourth_msg_pc2 ){
-  cout <<"in"<<endl;
-  pcs_msg_pc2.clear();
-  pcs_msg_pc2.push_back(first_msg_pc2);
-  pcs_msg_pc2.push_back(second_msg_pc2);
-  pcs_msg_pc2.push_back(third_msg_pc2);
-  pcs_msg_pc2.push_back(fourth_msg_pc2);
-  merge_pcs(pcs_msg_pc2);
+  pcs_msg_pc2_.clear();
+  pcs_msg_pc2_.push_back(first_msg_pc2);
+  pcs_msg_pc2_.push_back(second_msg_pc2);
+  pcs_msg_pc2_.push_back(third_msg_pc2);
+  pcs_msg_pc2_.push_back(fourth_msg_pc2);
+  merge_pcs(pcs_msg_pc2_);
 }
 void callback5(const PCMsg::ConstPtr& first_msg_pc2, const PCMsg::ConstPtr& second_msg_pc2, const PCMsg::ConstPtr& third_msg_pc2, const PCMsg::ConstPtr& fourth_msg_pc2, const PCMsg::ConstPtr& fifth_msg_pc2 ){
-  cout <<"in"<<endl;
-  pcs_msg_pc2.clear();
-  pcs_msg_pc2.push_back(first_msg_pc2);
-  pcs_msg_pc2.push_back(second_msg_pc2);
-  pcs_msg_pc2.push_back(third_msg_pc2);
-  pcs_msg_pc2.push_back(fourth_msg_pc2);
-  pcs_msg_pc2.push_back(fifth_msg_pc2);
-  merge_pcs(pcs_msg_pc2);
+  pcs_msg_pc2_.clear();
+  pcs_msg_pc2_.push_back(first_msg_pc2);
+  pcs_msg_pc2_.push_back(second_msg_pc2);
+  pcs_msg_pc2_.push_back(third_msg_pc2);
+  pcs_msg_pc2_.push_back(fourth_msg_pc2);
+  pcs_msg_pc2_.push_back(fifth_msg_pc2);
+  merge_pcs(pcs_msg_pc2_);
 }
 
 void merge_pcs(vector<PCMsg::ConstPtr> pcs_msg_pc2){
   
+  for(int i=0; i<pcs_msg_pc2.size(); i++){
+    // conversionns to pcl::PointCloud
+    pcl::fromROSMsg(*pcs_msg_pc2[i], *pcs_[i]);
+    
+    if(first_frame_)
+      // get transform between the pc frame and the output frame
+      get_transform(pcs_[i]->header.frame_id, transforms_[i]);
+    
+    // transform the pc to the output frame
+    pcl::transformPointCloud(*pcs_[i], *pcs_[i], transforms_[i].translation, transforms_[i].rotation);
+    pcs_[i]->header.frame_id = out_frame_;
+    
+    // sum the pcs
+    if(i==0)
+      *merged_pc_ = *pcs_[i];
+    else
+      *merged_pc_ += *pcs_[i];
+  }
+
+  first_frame_ = false;
+  pc_pub_.publish(*merged_pc_);
   
+}
+
+void get_transform(string pc_frame, pclTransform &pc_transform){
+   
+  tf::TransformListener trans_listener;
+  tf::StampedTransform stampedTransform;
+  
+  try{
+    trans_listener.waitForTransform(out_frame_, pc_frame, ros::Time(0), ros::Duration(10.0));
+    trans_listener.lookupTransform(out_frame_, pc_frame, ros::Time(0), stampedTransform);
+  }
+  catch(tf::TransformException &ex){
+    cout << ex.what() << endl;
+    ROS_ERROR("%s", ex.what());
+  }
+  tf::vectorTFToEigen(stampedTransform.getOrigin(), pc_transform.translation);      
+  tf::quaternionTFToEigen(stampedTransform.getRotation(), pc_transform.rotation);
+
+  return;
 }
